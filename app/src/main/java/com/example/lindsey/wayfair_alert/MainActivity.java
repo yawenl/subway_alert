72b6205b619d1e44bfe955a5995ff2200ecdb78e
Da Meng <dameng@wayfair.com>
2015-07-17 23:04:21 -0400

    first commit

--- app/src/main/java/com/example/lindsey/wayfair_alert/MainActivity.java
+++ app/src/main/java/com/example/lindsey/wayfair_alert/MainActivity.java
@@ -4,14 +4,44 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends ActionBarActivity {
+
+    // json object response url
+    private String urlJsonObj = "http://api.androidhive.info/volley/person_object.json";
+
+    // json array response url
+    private String urlJsonArry = "http://api.androidhive.info/volley/person_array.json";
+
+    // temporary string to show the parsed response
+    private String jsonResponse;
+    private static String TAG = MainActivity.class.getSimpleName();
+    private Button btnMakeObjectRequest;
+    private TextView txtResponse;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtResponse = (TextView) findViewById(R.id.txtResponse);
+        makeJsonObjectRequest();
     }
 
 
@@ -35,5 +65,48 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void makeJsonObjectRequest() {
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest
+                (Request.Method.GET,
+                        urlJsonObj,
+                        (String)null,
+                        new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
+
+                try {
+                    // Parsing json object response
+                    // response will be a json object
+                    String name = response.getString("name");
+                    String email = response.getString("email");
+                    JSONObject phone = response.getJSONObject("phone");
+                    String home = phone.getString("home");
+                    String mobile = phone.getString("mobile");
+
+                    jsonResponse = "";
+                    jsonResponse += "Name: " + name + "\n\n";
+                    jsonResponse += "Email: " + email + "\n\n";
+                    jsonResponse += "Home: " + home + "\n\n";
+                    jsonResponse += "Mobile: " + mobile + "\n\n";
+
+                    txtResponse.setText(jsonResponse);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+        // Adding request to request queue
+        APICallActivity.getInstance().addToRequestQueue(jsonObjReq);
     }
 }
