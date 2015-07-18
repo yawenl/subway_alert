9520e9bbd2bf5221384ac503ac57941af2d24d0a
yawenl <yawenluo@umich.edu>
2015-07-18 16:44:51 -0400

    showing up

--- app/src/main/java/com/example/lindsey/wayfair_alert/MainActivity.java
+++ app/src/main/java/com/example/lindsey/wayfair_alert/MainActivity.java
@@ -1,27 +1,51 @@
 package com.example.lindsey.wayfair_alert;
 
 
-import android.content.SharedPreferences;
-import android.graphics.Typeface;
-
-import android.os.AsyncTask;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Timer;
 
+import enumPackage.DirectionOptions;
+import enumPackage.StationOptions;
+
 public class MainActivity extends ActionBarActivity {
+
+    private int person_home_time_hour = 18;
+    private int person_home_time_min = 0;
+    private int train_arrive_work_station_hour = 8;
+    private int train_arrive_work_station_min = 0;
+
+    private String work_station_name;
+    private String line;
+    private String direction_name;
+    private int work_station_walk_time = 0;
+
+    private Button time_to_work_station_button;
+    private Button time_to_home_station_button;
+    private Button go_to_home_time_button;
+    private Button station_name_button;
+    private TextView leave_work_text;
+    private TextView leave_home_text;
+    private TextView train_come_work_text;
+
+    private Map<Integer, String> color_map = new HashMap<Integer, String>();
 
     private ProgressBar progressBar;
     private TrainInfo train_info;
@@ -32,11 +56,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //Fix the issue Android changes the font of password fields into monospace
-        EditText passField = (EditText) findViewById(R.id.pword);
-        passField.setTypeface(Typeface.DEFAULT);
+        setContentView(R.layout.activity_index);
 
         progressBar = (ProgressBar) findViewById(R.id.spinner);
 
@@ -50,8 +70,63 @@ public class MainActivity extends ActionBarActivity {
         GenerateAlert ga = new GenerateAlert(train_info, this);
         timer.schedule(ga, 0, 5000);
 
+        color_map.put(0, "FFAD5C");
+        color_map.put(1, "94FF94");
+        color_map.put(2, "A9E9FF");
+        color_map.put(3, "FF5C5C");
+        getValues();
+        setTrainArriveTime();
+        setWorkEndWorkTime();
+        setStationAndDirection();
+        //setWalkTime();
         Log.d("main:", train_info.notification);
        // createNotification(train_info.notification);
+    }
+
+    public void getValues() {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        //now get Editor
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        this.person_home_time_hour = sharedPref.getInt("homeHour", 18);
+        this.person_home_time_min = sharedPref.getInt("homeMinute", 0);
+        //this.home_station_walk_time = sharedPref.getInt("home_station_walk_time", 5);
+        this.work_station_walk_time = sharedPref.getInt("work_station_walk_time", 5);
+        this.work_station_name = StationOptions.val(sharedPref.getInt("station", 1));
+        this.direction_name = DirectionOptions.val(sharedPref.getInt("direction", 1));
+        this.line = color_map.get(sharedPref.getInt("line", 0));
+    }
+
+    public void setTrainArriveTime() {
+        this.train_come_work_text = (TextView)findViewById(R.id.train_arrive_time_work);
+        this.train_come_work_text.setText(""+train_arrive_work_station_hour + ":" + ((train_arrive_work_station_min < 10) ? ("0"+train_arrive_work_station_min) : train_arrive_work_station_min));
+    }
+
+    public void setWorkEndWorkTime() {
+        this.go_to_home_time_button = (Button) findViewById(R.id.go_to_home_time);
+        this.go_to_home_time_button.setText("" + person_home_time_hour + ":" +
+                ((person_home_time_min < 10) ? ("0" + person_home_time_min) : person_home_time_min));
+    }
+
+    public void setStationAndDirection() {
+        this.station_name_button = (Button)findViewById(R.id.work_station_name);
+        this.station_name_button.setText(this.work_station_name + " - " + this.direction_name);
+        this.station_name_button.setTextColor(Integer.parseInt(this.line,16));
+    }
+
+    public void setWalkTime() {
+        this.time_to_work_station_button = (Button) findViewById(R.id.time_to_work_station);
+        this.time_to_work_station_button.setText("Time needed to get to station: " + this.work_station_walk_time);
+    }
+
+    public void gotoTimeSetting(View view) {
+        Intent timeSettingIntent = new Intent(this, SetStartTimeActivity.class);
+        startActivity(timeSettingIntent);
+    }
+
+    public void gotoStationSetting(View view) {
+        Intent stationSettingIntent = new Intent(this, SetStopAndDirectionActivity.class);
+        startActivity(stationSettingIntent);
     }
 
     @Override
@@ -105,24 +180,6 @@ public class MainActivity extends ActionBarActivity {
     }
 
     /**
-     * Callback function for the Login Button. Synchronously signs in the user.
-     * @param view
-     */
-    public void logIn(View view) {
-        //prevent multiple login clicks
-        if (progressBar.getVisibility() == View.VISIBLE)
-            return;
-        progressBar.setVisibility(View.VISIBLE);
-
-        EditText usernameField = (EditText) findViewById(R.id.uname);
-        EditText passField = (EditText) findViewById(R.id.pword);
-
-        //Use AsyncTask to enable built-in Espresso support for testing async operations
-        AsyncTask<String, Void, String> loginTask = createLoginAsyncTask();
-        loginTask.execute(usernameField.getText().toString(), passField.getText().toString());
-    }
-
-    /**
      * Private function that creates an async task for login.
      * @return AsyncTask
      */
@@ -148,26 +205,11 @@ public class MainActivity extends ActionBarActivity {
             @Override
             protected void onPostExecute(String errorMsg) {
                 progressBar.setVisibility(View.GONE);
-                if (errorMsg == null) { //login success then launch main activity
                     finish();
-                } else {
-                    Log.d("MyApp", errorMsg);
-                    Utility.warningDialog(MainActivity.this, "Login Failed", errorMsg);
-                }
             }
         };
     }
 
-
-    /**
-     * Callback function for signUp button. Triggers an Intent to go to the SignUpActivity.
-     * @param view
-     */
-    public void gotoSignUp(View view) {
-        Intent signUpIntent = new Intent(this, SignUpActivity.class);
-        startActivity(signUpIntent);
-        finish();   //destroy activity to prevent going back to sign-in screen
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
