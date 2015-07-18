27cc975ec2097bfa2cc67dfa0b5be849742ff9fe
Lindsey Liu <jmjemi327@gmail.com>
2015-07-17 20:06:47 -0400

    parse

---
+++ app/src/main/java/com/example/lindsey/wayfair_alert/SubwayAlertApp.java
@@ -0,0 +1,98 @@
+package com.example.lindsey.wayfair_alert;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.parse.ParseFacebookUtils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by lindsey on 15-07-17.
+ */
+public class SubwayAlertApp extends Application {
+    public Activity currentActivity;    //tracks the activity on display in the foreground
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //Initialize Parse API to initialize connection to cloud
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "J6Zjvn0eTBaH8idTVZFXCjnyOVBMg0175McsXNtD", "lsVIf14CSs17EqGHRl6Kfs9i9z9hfX1aCbzRuMu4");
+
+        // register to be informed of activities starting up
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity,
+                                          Bundle savedInstanceState) {
+                // new activity created; force its orientation to portrait
+                activity.setRequestedOrientation(
+                        ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+                //Every activity must call onResumed() before going to the foreground
+                currentActivity = activity;
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+                if (activity == currentActivity)
+                    currentActivity = null;
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+                if (activity == currentActivity)
+                    currentActivity = null;
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+                if (activity == currentActivity)
+                    currentActivity = null;
+            }
+        });
+        printKeyHash();
+    }
+
+    /**
+     * Call this method inside onCreate once to get your hash key
+     */
+    public void printKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.android.memeinn", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("subway", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+}
+
