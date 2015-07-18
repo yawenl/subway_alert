8a2da6499cd48048891f05935cb737a0660fce06
Da Meng <dameng@wayfair.com>
2015-07-18 09:45:32 -0400

    finish call

---
+++ app/src/main/java/com/example/lindsey/wayfair_alert/JSONParser.java
@@ -0,0 +1,80 @@
+package com.example.lindsey.wayfair_alert;
+
+/**
+ * Created by dameng on 7/18/2015.
+ */
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import android.util.Log;
+
+public class JSONParser {
+
+    static InputStream is = null;
+    static JSONObject json = null;
+    static String outPut = "";
+
+    // constructor
+    public JSONParser() {
+
+    }
+
+    public JSONObject getJSONFromUrl(String url) {
+
+        // Making the HTTP request
+        try {
+
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpGet httpPost = new HttpGet(url);
+
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            is = httpEntity.getContent();
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            outPut = sb.toString();
+            Log.e("JSON", outPut);
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+
+        try {
+            json = new JSONObject(outPut);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        // return JSON String
+        return json;
+
+    }
+}
--- app/src/main/java/com/example/lindsey/wayfair_alert/MainActivity.java
+++ app/src/main/java/com/example/lindsey/wayfair_alert/MainActivity.java
@@ -1,33 +1,62 @@
 package com.example.lindsey.wayfair_alert;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.android.volley.Request;
-import com.android.volley.Request.Method;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.example.lindsey.wayfair_alert.APICallActivity;
+import com.example.lindsey.wayfair_alert.JSONParser;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONTokener;
+
+import android.view.View;
+
+
 public class MainActivity extends ActionBarActivity {
 
-    // json object response url
-    private String urlJsonObj = "http://api.androidhive.info/volley/person_object.json";
-
-    // json array response url
-    private String urlJsonArry = "http://api.androidhive.info/volley/person_array.json";
+    private JSONParser jsonParser;
+    //10.0.2.2 is the address used by the Android emulators to refer to the host address
+    // change this to the IP of another host if required
+    private static String ageURL = "https://raw.githubusercontent.com/dm37537/Career-Matcher/master/App/test.json";
+    private static String getAge = "getAge";
+    private static String jsonResult = "success";
+    String uname;
+    String age_res;
+    TextView Results;
+    JSONObject json;
 
     // temporary string to show the parsed response
     private String jsonResponse;
@@ -40,8 +69,71 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtResponse = (TextView) findViewById(R.id.txtResponse);
-        makeJsonObjectRequest();
+
+        jsonParser = new JSONParser();
+        Results = (TextView) findViewById(R.id.txtResponse);
+        new GenerateNotification(ageURL).execute();
+
+
+    }
+
+    private class GenerateNotification extends AsyncTask<String, Void, String> implements Runnable{
+        private String url;
+
+        public GenerateNotification(String url) {
+            this.url = url;
+        }
+        public void run() {
+            doInBackground();
+        }
+        protected String doInBackground(String... urls) {
+            json = jsonParser.getJSONFromUrl(this.url);
+            String notification = "";
+            try {
+                int predict_arrival_time = 0;
+                String stop_id = json.getString("stop_id");
+                String stop_name = json.getString("stop_name");
+                String trip_name = "";
+                JSONArray modes = json.getJSONArray("mode");
+                for (int i = 0; i < modes.length(); ++i) {
+                    JSONObject mode = (JSONObject) modes.get(i);
+                    int type = mode.getInt("route_type");
+                    if (type == 1){
+                        JSONArray routes = mode.getJSONArray("route");
+                        for (int j = 0; j < routes.length(); ++j) {
+                            JSONObject route = (JSONObject) routes.get(i);
+                            String route_id = route.getString("route_id");
+                            JSONArray directions = route.getJSONArray("direction");
+                            for (int k = 0; k < directions.length(); ++k) {
+                                JSONObject direction = (JSONObject) directions.get(i);
+                                int direction_id = direction.getInt("direction_id");
+                                JSONArray trips = direction.getJSONArray("trip");
+                                for (int m = 0; m < trips.length(); ++m) {
+                                    JSONObject trip = (JSONObject) trips.get(i);
+                                    predict_arrival_time = trip.getInt("pre_dt");
+                                    trip_name = trip.getString("trip_name");
+                                }
+                            }
+                        }
+                    }
+                }
+
+                notification = "Train from " + trip_name + " will arrive soon";
+                Log.d(TAG, json.toString());
+                Log.d(TAG, notification);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Results.setText("There is no train available now");
+                return "";
+            }
+            Results.setText(notification);
+            return notification;
+        }
+    }
+
+    public JSONObject getAge(){
+        JSONObject json = jsonParser.getJSONFromUrl(ageURL);
+        return json;
     }
 
 
@@ -67,46 +159,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void makeJsonObjectRequest() {
-
-        JsonObjectRequest jsonObjReq = new JsonObjectRequest
-                (Request.Method.GET,
-                        urlJsonObj,
-                        (String)null,
-                        new Response.Listener<JSONObject>() {
-
-            @Override
-            public void onResponse(JSONObject response) {
-                Log.d(TAG, response.toString());
-
-                try {
-                    // Parsing json object response
-                    // response will be a json object
-                    String name = response.getString("name");
-                    String email = response.getString("email");
-                    JSONObject phone = response.getJSONObject("phone");
-                    String home = phone.getString("home");
-                    String mobile = phone.getString("mobile");
-
-                    jsonResponse = "";
-                    jsonResponse += "Name: " + name + "\n\n";
-                    jsonResponse += "Email: " + email + "\n\n";
-                    jsonResponse += "Home: " + home + "\n\n";
-                    jsonResponse += "Mobile: " + mobile + "\n\n";
-
-                    txtResponse.setText(jsonResponse);
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-            }
-        });
-
-        // Adding request to request queue
-        APICallActivity.getInstance().addToRequestQueue(jsonObjReq);
-    }
 }
