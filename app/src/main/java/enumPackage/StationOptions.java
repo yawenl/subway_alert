218082a38bfa90ba624181cde2e13557508d280b
yawenl <yawenluo@umich.edu>
2015-07-18 15:09:44 -0400

    backend seems linked

--- app/src/main/java/enumPackage/StationOptions.java
+++ app/src/main/java/enumPackage/StationOptions.java
@@ -6,5 +6,16 @@ package enumPackage;
  * Created by lubron on 7/18/15.
  */
 public enum StationOptions {
-    Copley, BackBay
+    Copley,
+    BackBay;
+
+    public static String val(int x) {
+        switch(x) {
+            case 0:
+                return enumPackage.StationOptions.Copley.name();
+            case 1:
+                return enumPackage.StationOptions.BackBay.name();
+        }
+        return enumPackage.StationOptions.Copley.name();
+    }
 }
