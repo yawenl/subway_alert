282f63a04dc6467d7f2559026f6dbba832b6f010
Da Meng <dameng@wayfair.com>
2015-07-19 12:16:49 -0400

    fix alert

--- app/src/main/java/com/example/lindsey/wayfair_alert/SnoozeNotifier.java
+++ app/src/main/java/com/example/lindsey/wayfair_alert/SnoozeNotifier.java
@@ -2,6 +2,7 @@ package com.example.lindsey.wayfair_alert;
 
 import android.app.Activity;
 import android.app.NotificationManager;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -13,16 +14,21 @@ public class SnoozeNotifier extends Activity {
         super.onCreate(savedInstanceState);
         NotificationManager nm = (NotificationManager)
                 getSystemService(NOTIFICATION_SERVICE);
-        //nm.cancel(getIntent().getExtras().getInt("notificationID"));
+        nm.cancel(getIntent().getExtras().getInt("notificationID"));
 
         // Create object of SharedPreferences.
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         //now get Editor
         SharedPreferences.Editor editor = sharedPref.edit();
         //put your value
-        editor.putString("isSnooze", "snoozed");
+        editor.putString("isDismiss", "skip");
         //commits your edits
         editor.commit();
+
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
 
         finish();
     }
